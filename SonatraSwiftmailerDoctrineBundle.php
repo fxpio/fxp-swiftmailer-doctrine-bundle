c40225fe9759e9f19c79e5e8cf465be2ef874384
François Pluchino <francois.pluchino@sonatra.com>
2015-05-15 17:03:05 +0200

    Add bundle

---
+++ SonatraSwiftmailerDoctrineBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\SwiftmailerDoctrineBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class SonatraSwiftmailerDoctrineBundle extends Bundle
+{
+}
