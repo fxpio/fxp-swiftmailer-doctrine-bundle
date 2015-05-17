1a1dec22739f03b4a5933eee88b7e2479118ca8c
François Pluchino <francois.pluchino@sonatra.com>
2015-05-17 14:31:47 +0200

    Add configuration tests

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\SwiftmailerDoctrineBundle\Tests\DependencyInjection;
+
+use Sonatra\Bundle\SwiftmailerDoctrineBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Processor;
+
+/**
+ * Configuration Tests.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEmptyConfiguration()
+    {
+        $process = new Processor();
+        $configs = array();
+        $validConfig = array(
+            'spool_email_class' => 'Sonatra\Bundle\SwiftmailerDoctrineBundle\Model\SpoolEmailInterface',
+            'override_send_command' => true,
+        );
+
+        $config = new Configuration();
+        $res = $process->process($config->getConfigTreeBuilder()->buildTree(), $configs);
+        $this->assertEquals($validConfig, $res);
+    }
+}
